d7271c95ccc113ef3b5d685c7991bef95dec2b41
satwaj <satwajbachhav2004@gmail.com>
2026-02-21 13:13:17 +0530

    post feature done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -19,7 +19,7 @@ const imagekit = new ImageKit ({
     return res.status(401).json({message:"Token not provided , Unauthorized Access"})
   }
 
-  let decoded = null
+  let decoded 
   try{
     decoded = jwt.verify(token, process.env.JWT_SECRET)
   }
@@ -56,7 +56,106 @@ const imagekit = new ImageKit ({
 
 
 
+
+
+async function getPostController (req,res){
+
+  const token = req.cookies.token
+
+  if(!token){
+    return res.status(401).json
+    ({
+      message:"Unauthorized Access"
+    })
+  }
+
+
+let decoded
+
+try
+{
+  decoded = jwt.verify(token , process.env.JWT_SECRET)
+}
+catch(err)
+{
+  return res.status(401).json({message:"Invalid Token"})
+}
+
+const userId = decoded.id
+
+const posts = await postModel.find({
+  user:userId
+})
+
+res.status(200).json({
+  message:"Posts fetched successfully",
+  posts
+})
+
+}
+
+
+
+
+
+async function getPostDetailsController (req,res){
+
+   console.log(req.body,req.file)
+
+  const token = req.cookies.token
+
+  if(!token){
+    return res.status(401).json
+    ({
+      message:"Token not provided , Unauthorized Access"
+    })
+  }
+
+  let decoded 
+
+  try{
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
+  }
+
+  catch(err){
+    return res.status(401).json
+    ({
+      message:"Invalid Token , Unauthorized Access"
+    })
+  }
+
+  const userId = decoded.id
+
+  const postId = req.params.postId
+
+  const post = await postModel.findById(postId)
+
+  if(!post){
+    return res.status(404).json({
+      message:"Post not found"
+    })
+  }
+
+
+  const isValidUser = post.user.toString() ===   userId
+
+  if(!isValidUser){
+    return res.status(403).json({
+      message:"Forbidden Content"
+    })
+  }
+
+  return res.status(200).json({
+    message:"Post details fetched successfully",
+    post
+  })
+
+
+}
+
 module.exports = {
-  createPostController
+  createPostController,
+  getPostController,
+  getPostDetailsController
 }
 
